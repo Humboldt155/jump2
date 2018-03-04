1f3ac0cf2c01774d4796fd848b15eea66ab3bbe3
Humboldt155 <petr_kozyrev@icloud.com>
2018-03-04 23:07:30 +0300

    16

--- src/store/modules/model-opus.js
+++ src/store/modules/model-opus.js
@@ -1,126 +1,126 @@
 import axios from 'axios'
 
-const excludeAtt = [
-  'Current national selling price has a promotion',
-  'Язык упаковки',
-  'Код ОКПД',
-  'International ID',
-  'Тип поставщика',
-  'Source country code',
-  'Тип AEE:',
-  'На какой склад?',
-  'Название',
-  'Модель продукта',
-  'Срок действия серт. пожарн. безоп. (до)',
-  'Производство Европейский Союз',
-  'Unit capacity',
-  'Current national selling price, taking into account the national promotions, including VAT',
-  'Current standard national selling price (not taking into account the promotions), including VAT',
-  'Current standard store selling price (not taking into account the promotions), including VAT',
-  'Current store selling price has a promotion',
-  'Current store selling price, taking into account the store promotions, including VAT',
-  'Administrative label',
-  'Available for sale stock',
-  'Base VAT code of the product for a specific store',
-  'Category ID of the management nomenclature',
-  'Customer short label',
-  'Date when the product status will be change to "to be removed"',
-  'Date when the storage of the product in warehouse is stopped',
-  'Department ID of the management nomenclature',
-  'Enabled GTIN values',
-  'GTIN links',
-  'IS eligible for web publication?',
-  'Is a LM store brand ?',
-  'Is a store brand ?',
-  'Is new ?',
-  'Is ranged product ?',
-  'Last store stock update date',
-  'Last update date',
-  'Letter range (Gamme)',
-  'Link to the base VAT code of the product',
-  'Link to the selling prices currency',
-  'Logistic depth',
-  'Logistic height',
-  'Logistic net weight',
-  'Номер модели',
-  'Logistic width',
-  'Management maturity level',
-  'Maturity level quality',
-  'Next standard national selling price (not taking into account the promotions), including VAT',
-  'Next standard store selling price (not taking into account the promotions), including VAT',
-  'Price repartition per quartile',
-  'Product status',
-  'Product typology',
-  'Publication maturity level',
-  'Re-stocking',
-  'Re-stocking recommendation',
-  'Sale status for the product',
-  'Selling prices currency',
-  'Start date of the next standard national selling price (not taking into account the promotions)',
-  'Start date of the next standard store selling price (not taking into account the promotions)',
-  'Sub-category ID of the management nomenclature',
-  'Sub-department ID of the management nomenclature',
-  'Substitution Products',
-  'Unit sale (for consumer)',
-  'Web publishing real status coming from publishing system',
-  'Квартиль',
-  'Классификация',
-  'Короткое наименование товара для клиента',
-  'Международ. Клас 1',
-  'Модель Адео',
-  'Область распространения',
-  'Поток',
-  'Стоп-Заказ',
-  'Тип гаммы',
-  'Ценник',
-  'Центральный сток',
-  'Additional links to accessory products',
-  'Additional links to consumables',
-  'Additional links to customer purchases',
-  'Additional links to implementations',
-  'Additional links to maintenance',
-  'Additional links to services',
-  'Additional links to tools',
-  'Amount of discount on the unit price, including VAT for the next (or current) store promotion',
-  'Capacity quantity',
-  'Current store selling price alternative capacity unit code',
-  'Current store selling price, expressed by alternative capacity unit',
-  'Distribution region',
-  'End date of the next (or current) store promotion',
-  'Flash sale category',
-  'Gap code of the next (or current) store promotion',
-  'Identifier of the commercial operation (OPECOM) linked to the next (or current) store promotion',
-  'Is 20/80 ?',
-  'Is part of composition ?',
-  'Links from product to samples',
-  'New unit price (including VAT) for the next (or current) store promotion',
-  'Percentage of discount of the next (or current) store promotion',
-  'Price category',
-  'Publication maturity level',
-  'Complementary products (cross sell)',
-  'Image',
-  'Searchable',
-  'Visible',
-  'Срок действия санит. епидем. (до)',
-  'Срок действия сертиф. соотв.(до)',
-  '№ Санитарно епидемиологич. заключение',
-  '№ Сертификат пожарной безопасности',
-  '№ Сертификата соответствия',
-  'LAD Category',
-  'Is editable selling price by store?',
-  'Is the product removed ?',
-  'ADR Risk classification',
-  'Expiration date category',
-  'Number of package (if multi package product)',
-  'Specific transport family',
-  'Start date of the next (or current) store promotion',
-  'Type of discount for the next (or current) store promotion',
-  'Comment for order lock reason',
-  'Order lock reason',
-  'Sale lock reason',
-  'Link from product to sample',
-  'Store stock recommendation'
-]
+// const excludeAtt = [
+//   'Current national selling price has a promotion',
+//   'Язык упаковки',
+//   'Код ОКПД',
+//   'International ID',
+//   'Тип поставщика',
+//   'Source country code',
+//   'Тип AEE:',
+//   'На какой склад?',
+//   'Название',
+//   'Модель продукта',
+//   'Срок действия серт. пожарн. безоп. (до)',
+//   'Производство Европейский Союз',
+//   'Unit capacity',
+//   'Current national selling price, taking into account the national promotions, including VAT',
+//   'Current standard national selling price (not taking into account the promotions), including VAT',
+//   'Current standard store selling price (not taking into account the promotions), including VAT',
+//   'Current store selling price has a promotion',
+//   'Current store selling price, taking into account the store promotions, including VAT',
+//   'Administrative label',
+//   'Available for sale stock',
+//   'Base VAT code of the product for a specific store',
+//   'Category ID of the management nomenclature',
+//   'Customer short label',
+//   'Date when the product status will be change to "to be removed"',
+//   'Date when the storage of the product in warehouse is stopped',
+//   'Department ID of the management nomenclature',
+//   'Enabled GTIN values',
+//   'GTIN links',
+//   'IS eligible for web publication?',
+//   'Is a LM store brand ?',
+//   'Is a store brand ?',
+//   'Is new ?',
+//   'Is ranged product ?',
+//   'Last store stock update date',
+//   'Last update date',
+//   'Letter range (Gamme)',
+//   'Link to the base VAT code of the product',
+//   'Link to the selling prices currency',
+//   'Logistic depth',
+//   'Logistic height',
+//   'Logistic net weight',
+//   'Номер модели',
+//   'Logistic width',
+//   'Management maturity level',
+//   'Maturity level quality',
+//   'Next standard national selling price (not taking into account the promotions), including VAT',
+//   'Next standard store selling price (not taking into account the promotions), including VAT',
+//   'Price repartition per quartile',
+//   'Product status',
+//   'Product typology',
+//   'Publication maturity level',
+//   'Re-stocking',
+//   'Re-stocking recommendation',
+//   'Sale status for the product',
+//   'Selling prices currency',
+//   'Start date of the next standard national selling price (not taking into account the promotions)',
+//   'Start date of the next standard store selling price (not taking into account the promotions)',
+//   'Sub-category ID of the management nomenclature',
+//   'Sub-department ID of the management nomenclature',
+//   'Substitution Products',
+//   'Unit sale (for consumer)',
+//   'Web publishing real status coming from publishing system',
+//   'Квартиль',
+//   'Классификация',
+//   'Короткое наименование товара для клиента',
+//   'Международ. Клас 1',
+//   'Модель Адео',
+//   'Область распространения',
+//   'Поток',
+//   'Стоп-Заказ',
+//   'Тип гаммы',
+//   'Ценник',
+//   'Центральный сток',
+//   'Additional links to accessory products',
+//   'Additional links to consumables',
+//   'Additional links to customer purchases',
+//   'Additional links to implementations',
+//   'Additional links to maintenance',
+//   'Additional links to services',
+//   'Additional links to tools',
+//   'Amount of discount on the unit price, including VAT for the next (or current) store promotion',
+//   'Capacity quantity',
+//   'Current store selling price alternative capacity unit code',
+//   'Current store selling price, expressed by alternative capacity unit',
+//   'Distribution region',
+//   'End date of the next (or current) store promotion',
+//   'Flash sale category',
+//   'Gap code of the next (or current) store promotion',
+//   'Identifier of the commercial operation (OPECOM) linked to the next (or current) store promotion',
+//   'Is 20/80 ?',
+//   'Is part of composition ?',
+//   'Links from product to samples',
+//   'New unit price (including VAT) for the next (or current) store promotion',
+//   'Percentage of discount of the next (or current) store promotion',
+//   'Price category',
+//   'Publication maturity level',
+//   'Complementary products (cross sell)',
+//   'Image',
+//   'Searchable',
+//   'Visible',
+//   'Срок действия санит. епидем. (до)',
+//   'Срок действия сертиф. соотв.(до)',
+//   '№ Санитарно епидемиологич. заключение',
+//   '№ Сертификат пожарной безопасности',
+//   '№ Сертификата соответствия',
+//   'LAD Category',
+//   'Is editable selling price by store?',
+//   'Is the product removed ?',
+//   'ADR Risk classification',
+//   'Expiration date category',
+//   'Number of package (if multi package product)',
+//   'Specific transport family',
+//   'Start date of the next (or current) store promotion',
+//   'Type of discount for the next (or current) store promotion',
+//   'Comment for order lock reason',
+//   'Order lock reason',
+//   'Sale lock reason',
+//   'Link from product to sample',
+//   'Store stock recommendation'
+// ]
 
 const firstCols = ['Код продукта', 'Название на сайте', 'Отдел', 'Дата AVS', 'Описание']
 
@@ -196,7 +196,7 @@ const actions = {
   },
   setProducts (vuexContext, modelId) {
     vuexContext.commit('setIsLoaded', true)
-    axios.get('https://webtopdata2.lmru.opus.adeo.com:5000/business/v2/products?pageSize=500&startFrom=1&filter=modelCode%3A'
+    axios.get('https://webtopdata2.lmru.opus.adeo.com:5000/business/v2/products?pageSize=100&startFrom=1&filter=modelCode%3A'
       .concat(modelId, '&expand=attributes&context=lang%3Aru'), {
       headers: {
         'Authorization': 'Basic d2lrZW86b2VraXc',
@@ -206,6 +206,8 @@ const actions = {
       .then(response => {
         const resp = response.data.content
         // Финальный лист всех продуктов в формате {key: value}
+
+        vuexContext.commit('setProductsTest', resp)
         let productsNew = []
 
         // Временный сет, в котором будем хранить все возможные значения атрибутов
@@ -217,27 +219,24 @@ const actions = {
           let product = {}
 
           // Лист всех атрибутов выбранного продукта
-          let attributes = resp[i].attribute
+          let attributes = resp[i].attribute.filter(function (attribute) {
+            let check = (attribute.href.slice(att_code_slice).split('@')[0].length === 5)
+            return check
+          })
 
           for (let j = 0; j < attributes.length; j++) {
             let attName = attributes[j].displayName
             console.log(attributes[j].displayName)
             // Пропускаем все атрибуты, находящиеся в списке 'excludeAtt'
-            if (excludeAtt.includes(attName)) { continue }
+            // if (excludeAtt.includes(attName)) { continue }
 
             // Вырезаем код атрибута из параметра href
             let attCode = 'ATT_'.concat(attributes[j].href.slice(att_code_slice)).split('@')[0]
-            if (attCode === 'ATT_maturityLevelPublication') { continue }
-            if (attCode === 'ATT_marketingDescription') { continue }
             let attConcat = attCode.concat(' - ', attName)
 
-            if (attCode === 'ATT_logistic-grossWeight') { attConcat = 'Вес, кг' }
             if (attCode === 'ATT_productID') { attConcat = 'Код продукта' }
-            if (attCode === 'ATT_3') { attConcat = 'Отдел' }
-            if (attCode === 'ATT_6') { attConcat = 'Дата AVS' }
-            if (attCode === 'ATT_12963') { attConcat = 'Название на сайте' }
-            if (attCode === 'ATT_marketingDescription') { attConcat = 'Описание' }
-            if (attCode === 'ATT_01022') { attConcat = 'Описание' }
+            if (attCode === 'ATT_12963') { attConcat = 'ATT_12963 - Название на сайте' }
+            if (attCode === 'ATT_01022') { attConcat = 'ATT_01022 - Описание' }
 
             let attValue = attributes[j].value[0]
             product[attConcat] = attValue
@@ -253,10 +252,10 @@ const actions = {
           let product = productsNew[i]
           for (let key of att) {
             if ((key in product)) {
-              if (key === 'Описание') { pQ['description'] += 1 }
+              if (key === 'ATT_01022 - Описание') { pQ['description'] += 1 }
             }
             if (!(key in product)) {
-              if (key === 'Описание') {
+              if (key === 'ATT_01022 - Описание') {
                 productsNew[i][key] = 'Нет описания'
               } else if (key === 'Дата AVS') {
                 pQ['avs'] += 1 // + Товар без avs
@@ -271,7 +270,7 @@ const actions = {
           'Название на сайте': 'Название на сайте',
           'Отдел': 'Отдел',
           'Дата AVS': 'Дата AVS',
-          'Описание': 'Описание'
+          'ATT_01022 - Описание': 'ATT_01022 - Описание'
         }
         let fields = [
           {
@@ -294,7 +293,7 @@ const actions = {
             label: 'Дата AVS',
             sortable: true
           },
-          'Описание'
+          'ATT_01022 - Описание'
         ]
         for (let key of att) {
           if (!(firstCols.includes(key))) {
