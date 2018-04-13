cd37e185f5ca23ae7512fab33a0adbb19cbbc8e8
Humboldt155 <petr_kozyrev@icloud.com>
2018-04-13 15:59:40 +0300

    148

--- src/components/family.vue
+++ src/components/family.vue
@@ -1,16 +1,38 @@
 <template>
     <b-container fluid>
+      <br>
       <b-row>
         <b-col>
-          <p>Семьи</p>
-          <b-btn size="sm" variant="info" @click="setFamilies">Список семей</b-btn>
+          <b-col>
+            Загружено {{ families.length }} семей
+          </b-col>
         </b-col>
       </b-row>
+      <br>
       <b-row>
-        <b-col>
-          {{ families }}
+        <b-col cols="8">
+          <b-input-group prepend="Семья">
+            <b-form-input v-model.lazy="searchText" placeholder="введите название и нажмите Начать анализ"></b-form-input>
+            <b-input-group-append>
+              <b-btn variant="secondary">Начать анализ</b-btn>
+            </b-input-group-append>
+          </b-input-group>
         </b-col>
       </b-row>
+
+      <div v-if="searchText.length>=3">
+        <b-form-group>
+          <b-form-radio-group id="btnradios3"
+                              buttons
+                              button-variant="outline-secondary"
+                              stacked
+                              v-model="searchText"
+                              :options="options"
+                              name="radioBtnStacked">
+          </b-form-radio-group>
+        </b-form-group>
+      </div>
+      <div v-else></div>
     </b-container>
 </template>
 
@@ -19,18 +41,26 @@ export default {
   name: 'family',
   data () {
     return {
-      i: 0
-    }
-  },
-  methods: {
-    setFamilies () {
-      this.$store.dispatch('setFamilies')
+      searchText: ''
     }
   },
   computed: {
     families () {
       return this.$store.getters.families
+    },
+    options () {
+      let name = this.searchText.toString().toLowerCase()
+      if (this.families.length === 1) {
+        return [{value: 0, text: ''}]
+      } else {
+        return this.families.filter(function (f) {
+          return f.text.toString().toLowerCase().includes(name)
+        })
+      }
     }
+  },
+  mounted () {
+    this.$store.dispatch('setFamilies')
   }
 }
 </script>
--- src/store/modules/family.js
+++ src/store/modules/family.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // initial state
 const state = {
-  families: ''
+  families: []
 }
 
 // mutations
@@ -21,21 +21,23 @@ const getters = {
 const actions = {
   setFamilies (vuexContext) {
     let families = []
-    axios.get('https://webtopdata2.lmru.opus.adeo.com:5000/business/v2/families?pageSize=500&startFrom=1', {
-      headers: {
-        'Authorization': 'Basic d2lrZW86b2VraXc'
-      }
-    })
-      .then(response => {
-        const resp = response.data.content
-        // console.log(resp)
-        for (let i = 0; i < resp.length; i++) {
-          families.push(resp[i]['correlationId'])
+    for (let iter = 0; iter < 2; iter++) {
+      axios.get('https://webtopdata2.lmru.opus.adeo.com:5000/business/v2/families?pageSize=500&startFrom='.concat((1 + iter * 500).toString()), {
+        headers: {
+          'Authorization': 'Basic d2lrZW86b2VraXc'
         }
       })
-      .catch(e2 => {
-        this.errors.push(e2)
-      })
+        .then(response => {
+          const resp = response.data.content
+          // console.log(resp)
+          for (let i = 0; i < resp.length; i++) {
+            families.push({value: resp[i]['correlationId'], text: resp[i]['attribute'][0]['value'][0]})
+          }
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    }
     console.log(families.length)
     vuexContext.commit('setFamilies', families)
   }
